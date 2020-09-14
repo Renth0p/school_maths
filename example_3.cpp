2efdd5aeef8c6841ddf8705c84e7144cd0df9918
Renthop <44197289+Renth0p@users.noreply.github.com>
2020-09-14 23:20:43 +0300

    Create example_3.cpp

---
+++ example_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+setlocale(LC_ALL, "rus");
+double x= 3.74 * pow(10, -2);
+double y= -0.825;
+double z= 0.16 * pow(10, 2);
+double s;
+
+s= 1+pow(sin(x+y),2) / fabs(x - 2*y/1+pow(x,2)*pow(y,2)) * pow(x, fabs(3)) + pow(cos(atan(1/z)), 2);
+
+cout << "result = " << s;
+
+system("pause");
+return 0;
+}
