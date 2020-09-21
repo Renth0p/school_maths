3b5de73f8fbbc696d31a3d3b4c9b020e23cfb8b2
Renthop <44197289+Renth0p@users.noreply.github.com>
2020-09-21 17:04:51 +0300

    Update example_5.cpp

--- example_5.cpp
+++ example_5.cpp
@@ -11,12 +11,11 @@ double y= 4.642 * pow(10, -2);
 double z= 21;
 double s;
 
-s= log10(pow(y, -sqrt(fabs(x)))) * (x - y/2) + pow(sin(atan(z)),2);
+s= log(pow(y, -sqrt(fabs(x)))) * (x - y/2) + (pow(sin(atan(z)),2));
 
-cout << "result = " << s;
+cout « "result = " « s;
 
 system("pause");
 return 0;
 }
-
-//ответ не совпадает 
+// 5 пример
