ba83dd3ac35fc8322e1b474d337811df58a5f24b
Renthop <44197289+Renth0p@users.noreply.github.com>
2020-09-21 17:04:10 +0300

    Update example_3.cpp

--- example_3.cpp
+++ example_3.cpp
@@ -11,12 +11,12 @@ double y= -0.825;
 double z= 0.16 * pow(10, 2);
 double s;
 
-s= 1+pow(sin(x+y),2) / fabs(x - 2*y/1+pow(x,2)*pow(y,2)) * pow(x, fabs(3)) + pow(cos(atan(1/z)), 2);
+s= (1+pow(sin(x+y),2)) / fabs(x - 2*y/1+pow(x,2)*pow(y,2)) * pow(x, fabs(y)) + pow(cos(atan(1/z)), 2);
 
-cout << "result = " << s;
+cout « "result = " « s;
 
 system("pause");
 return 0;
 }
 
-//ответ не совпадает
+// 3 пример
